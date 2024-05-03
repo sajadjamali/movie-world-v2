dbddcbba8857d357bbd1b013ee726eb706807674
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-03 18:42:36 +0330

    .env edited

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,9 +4,7 @@ import { IAuthData } from "@/types/auth";
 export const baseUrl = "https://api.themoviedb.org/3";
 export const api_key = "a9a8b69054a2ff955dc152216931af1a";
 export const imgBaseUrl = "https://image.tmdb.org/t/p/w500";
-// const url = process.env.NEXT_PUBLIC_BASE_URL;
- 
-const url = 'https://movie-world-nine.vercel.app/api/';
+const url = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const getApi = async (slug: string) => {
     try {

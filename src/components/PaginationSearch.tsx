4ef23409302f138083c4d3bc863b658642cda2fa
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-05 22:31:46 +0330

    refactor...

--- src/components/PaginationSearch.tsx
+++ src/components/PaginationSearch.tsx
@@ -14,6 +14,7 @@ const PaginationSearch: React.FC<{ searchValue: string }> = ({ searchValue }) =>
 
     const [pageNumber, setPageNumber] = useState<number>(1);
     const [selectedItem, setSelectedItem] = useState<string>('1');
+
     const fetchUrl = getFetchUrl(searchValue, pageNumber, selectedItem);
     const { data, isLoading, isError } = useGetSearchedItems(searchValue, selectedItem, fetchUrl, pageNumber);
 
@@ -25,7 +26,7 @@ const PaginationSearch: React.FC<{ searchValue: string }> = ({ searchValue }) =>
     if (isError) return <p className="my-10 text-white text-center text-xl">error😑</p>
     if (!data.results.length) return (
         <div className="text-xl flex flex-col items-center space-y-10 text-white">
-            <p>Searched term: <span className="text-rose-500">{searchValue}</span></p>
+            <p>Searched text: <span className="text-rose-500">{searchValue}</span></p>
             <p>Unfortunately, there is no item with this name</p>
         </div>
     )
@@ -35,7 +36,7 @@ const PaginationSearch: React.FC<{ searchValue: string }> = ({ searchValue }) =>
     return (
         <>
             <div className="select relative flex h-[3em] overflow-hidden ring-2 ring-yellow-500 w-10/12 min-[400px]:w-6/12 sm:w-5/12 lg:w-3/12 2xl:w-2/12 mx-auto rounded-md">
-                <select onChange={(e) => setSelectedItem(e.target.value)}>
+                <select value={selectedItem} onChange={(e) => { setSelectedItem(e.target.value); setPageNumber(1) }}>
                     <option value="1">Movies</option>
                     <option value="2">Actors</option>
                 </select>
@@ -43,18 +44,21 @@ const PaginationSearch: React.FC<{ searchValue: string }> = ({ searchValue }) =>
             {
                 isExistPoster(data.results) ? (
                     selectedItem === '1' ? (
-                        <MoviesList title={`${convertToPascalCase(`Searched term: ${searchValue}`)}`} movies={data.results} />
+                        <MoviesList title={`${convertToPascalCase(`Searched text: ${searchValue}`)}`} movies={data.results} />
                     ) : (
-                        <div className="px-3 text-white mt-10 sm:mt-20 grid grid-cols-2 min-[400px]:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-9 md:gap-5 justify-items-center gap-3">
-                            {
-                                data.results.map((actor: ActorType) => (
-                                    actor.profile_path && <Actor key={actor.id} actor={actor} />
-                                ))
-                            }
-                        </div>
+                        <>
+                            <p className="text-white text-2xl text-center my-10">Searched text: <span className="text-rose-500">{searchValue}</span></p>
+                            <div className="px-3 text-white grid grid-cols-2 min-[400px]:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-9 md:gap-5 justify-items-center gap-3">
+                                {
+                                    data.results.map((actor: ActorType) => (
+                                        actor.profile_path && <Actor key={actor.id} actor={actor} />
+                                    ))
+                                }
+                            </div>
+                        </>
                     )
                 ) : (
-                    <p className="text-center text-rose-500 text-xl my-20">Unfortunately, there is no information on this page</p>
+                    <p className="text-center text-rose-500 text-xl my-20">there is no information on this pagination page</p>
                 )
             }
 

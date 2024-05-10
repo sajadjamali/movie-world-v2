86964a9e68cc14b022dd77a5be390af3bc25b304
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-10 22:08:46 +0330

    ready for deploy

--- prisma/migrations/20240510151050_init/migration.sql
+++ prisma/migrations/20240510151050_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "phone" TEXT NOT NULL
+    "phone" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex

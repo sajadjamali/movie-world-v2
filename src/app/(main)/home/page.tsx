d0dd805b52711f3fc21e0234fa62a27000b4ff2b
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-13 17:30:08 +0330

    suspense added

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,20 +1,31 @@
 import Slider from "@/components/Slider";
 import CategorySection from "@/components/CategorySection";
 import MyDivider from "@/components/muiComponents/MyDivider";
+import { Suspense } from 'react'
 
 const Page = async () => {
     return (
         <div className="pb-8">
-            <Slider />
+            <Suspense fallback={<p>Loading...</p>}>
+                <Slider />
+            </Suspense>
             <main className="pt-5">
                 <MyDivider label="Now Playing" src="nowPlaying.png" />
-                <CategorySection effect="" sectionName="now_playing" />
+                <Suspense fallback={<p>Loading...</p>}>
+                    <CategorySection effect="" sectionName="now_playing" />
+                </Suspense>
                 <MyDivider label="Popular" src="popular.png" />
-                <CategorySection effect="fade-right" sectionName="popular" />
+                <Suspense fallback={<p>Loading...</p>}>
+                    <CategorySection effect="fade-right" sectionName="popular" />
+                </Suspense>
                 <MyDivider label="Top Rated" src="topRated.png" />
-                <CategorySection effect="flip-left" sectionName="top_rated" />
+                <Suspense fallback={<p>Loading...</p>}>
+                    <CategorySection effect="flip-left" sectionName="top_rated" />
+                </Suspense>
                 <MyDivider label="Upcoming" src="upcoming.png" />
-                <CategorySection effect="zoom-in-up" sectionName="upcoming" />
+                <Suspense fallback={<p>Loading...</p>}>
+                    <CategorySection effect="zoom-in-up" sectionName="upcoming" />
+                </Suspense>
             </main>
         </div>
     );

b517123d100cc2d7f7efbe11bc555804cbc2367c
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-15 23:40:43 +0330

    Logout with cookies has been abandoned

--- src/app/api/auth/logOut/route.ts
+++ src/app/api/auth/logOut/route.ts
@@ -1,33 +1,30 @@
-// import { serialize } from 'cookie';
-
-var cookie = require('cookie');
-
-// export async function GET(request: Request) {
-//     try {
-//         return Response.json({ message: 'user logged out successfully' }, {
-//             status: 200,
-//             headers: { 'Set-Cookie': `token=; Path=/; Max-Age=0` }
-//         });
-
-//     } catch (error) {
-//         return Response.json({ message: 'Internal server error', error }, { status: 500 });
-//     }
-// }
-
-
 export async function GET(request: Request) {
     try {
         return Response.json({ message: 'user logged out successfully' }, {
             status: 200,
-            headers: {
-                'Set-Cookie': cookie.serialize('token', '', {
-                    path: '/',
-                    maxAge: 0
-                }),
-            }
+            headers: { 'Set-Cookie': `token=; Path=/; Max-Age=0` }
         });
 
     } catch (error) {
         return Response.json({ message: 'Internal server error', error }, { status: 500 });
     }
 }
+
+var cookie = require('cookie');
+//
+// export async function GET(request: Request) {
+//     try {
+//         return Response.json({ message: 'user logged out successfully' }, {
+//             status: 200,
+//             headers: {
+//                 'Set-Cookie': cookie.serialize('token', '', {
+//                     path: '/',
+//                     maxAge: 0
+//                 }),
+//             }
+//         });
+
+//     } catch (error) {
+//         return Response.json({ message: 'Internal server error', error }, { status: 500 });
+//     }
+// }

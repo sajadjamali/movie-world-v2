c96b2df40a1328545fafec6953e7361fbf7128a3
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-15 23:32:29 +0330

    add cookie package

--- src/app/api/auth/logOut/route.ts
+++ src/app/api/auth/logOut/route.ts
@@ -1,30 +1,33 @@
 // import { serialize } from 'cookie';
 
-export async function GET(request: Request) {
-    try {
-        return Response.json({ message: 'user logged out successfully' }, {
-            status: 200,
-            headers: { 'Set-Cookie': `token=; Path=/; Max-Age=0` }
-        });
-
-    } catch (error) {
-        return Response.json({ message: 'Internal server error', error }, { status: 500 });
-    }
-}
-
+var cookie = require('cookie');
 
 // export async function GET(request: Request) {
 //     try {
 //         return Response.json({ message: 'user logged out successfully' }, {
 //             status: 200,
-//             headers: {
-//                 'Set-Cookie': serialize('mytoken1', 'mytoken1Value', {
-//                     path: '/',
-//                 }),
-//             }
+//             headers: { 'Set-Cookie': `token=; Path=/; Max-Age=0` }
 //         });
 
 //     } catch (error) {
 //         return Response.json({ message: 'Internal server error', error }, { status: 500 });
 //     }
 // }
+
+
+export async function GET(request: Request) {
+    try {
+        return Response.json({ message: 'user logged out successfully' }, {
+            status: 200,
+            headers: {
+                'Set-Cookie': cookie.serialize('token', '', {
+                    path: '/',
+                    maxAge: 0
+                }),
+            }
+        });
+
+    } catch (error) {
+        return Response.json({ message: 'Internal server error', error }, { status: 500 });
+    }
+}

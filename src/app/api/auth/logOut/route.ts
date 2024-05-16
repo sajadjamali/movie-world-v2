58c74e607059880405583473dfb6aebb391f71fe
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-16 15:52:27 +0330

    bug of token

--- src/app/api/auth/logOut/route.ts
+++ src/app/api/auth/logOut/route.ts
@@ -11,12 +11,14 @@
 //     }
 // }
 
+import { cookies } from 'next/headers'
 import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
         const response = NextResponse.json({ message: 'user logged out successfully' });
-        response.cookies.delete('token');
+        // response.cookies.delete('token');
+        cookies().delete('token')
         return response;
     } catch (error) {
         return Response.json({ message: 'Internal server error', error }, { status: 500 });

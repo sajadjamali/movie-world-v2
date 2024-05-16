886900e5e1f867fa94cd512de095b8bd2582e00f
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-16 10:47:26 +0330

    bug of logout

--- src/layout/UserInfo.tsx
+++ src/layout/UserInfo.tsx
@@ -6,12 +6,18 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import PhonelinkRingIcon from '@mui/icons-material/PhonelinkRing';
 import { logOut } from '@/services/auth';
 
-const UserInfo: React.FC<{ user: ILoggedUser, logOut: () => void }> = ({ user }) => {
+const UserInfo: React.FC<{ user: ILoggedUser }> = ({ user }) => {
+
+    const handleClick = async () => {
+       const res = await logOut();
+       console.log(res.message)
+
+    }
 
     return (
         <div className='bg-gray-900 rounded-md ring-1 ring-yellow-400 text-base p-2 space-y-4 text-white'>
             <div className='flex items-center space-x-3'>
-                <Image className='rounded-full' src="/assets/imgs/profile.png" alt='profile' width={50} height={50} />
+                <Image loading='eager' className='rounded-full' src="/assets/imgs/profile.png" alt='profile' width={50} height={50} />
                 <p className='text-rose-500 text-lg tracking-wider'>{user.name}</p>
             </div>
             <hr />
@@ -24,7 +30,7 @@ const UserInfo: React.FC<{ user: ILoggedUser, logOut: () => void }> = ({ user })
                 <p>{user.phone}</p>
             </div>
             <hr />
-            <button onClick={() => logOut()} className='text-rose-500 animate-bounce hover:text-yellow-400 rounded-md block w-28 mx-auto text-center text-lg'>
+            <button onClick={() => handleClick()} className='text-rose-500 animate-bounce hover:text-yellow-400 rounded-md block w-28 mx-auto text-center text-lg'>
                 <LogoutIcon className='me-2 text-yellow-400' />
                 Log Out
             </button>

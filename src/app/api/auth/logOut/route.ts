54b3030f699696177eac8dd8958026dafa5be25e
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-16 14:28:02 +0330

    remove domain from cookie

--- src/app/api/auth/logOut/route.ts
+++ src/app/api/auth/logOut/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: Request) {
     try {
         return NextResponse.json({ message: 'user logged out successfully' }, {
             status: 200,
-            headers: { 'Set-Cookie': 'token=; HttpOnly; domain=movie-world-v2.vercel.app; Path=/; Max-Age=0' }
+            headers: { 'Set-Cookie': 'token=; HttpOnly; Path=/; Max-Age=0' }
         });
     } catch (error) {
         return NextResponse.json({ message: 'Internal server error', error }, { status: 500 });

93eac9b032424d20d33dbb6ffa84cc571e90658b
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-16 14:24:57 +0330

    change Responce to NextResponse

--- src/app/api/auth/logOut/route.ts
+++ src/app/api/auth/logOut/route.ts
@@ -1,22 +1,24 @@
-// export async function GET(request: Request) {
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    try {
+        return NextResponse.json({ message: 'user logged out successfully' }, {
+            status: 200,
+            headers: { 'Set-Cookie': 'token=; HttpOnly; domain=movie-world-v2.vercel.app; Path=/; Max-Age=0' }
+        });
+    } catch (error) {
+        return NextResponse.json({ message: 'Internal server error', error }, { status: 500 });
+    }
+}
+
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
 //     try {
-//         return Response.json({ message: 'user logged out successfully' }, {
-//             status: 200,
-//             headers: { 'Set-Cookie': 'token=; HttpOnly; domain=movie-world-v2.vercel.app; Path=/; Max-Age=0' }
-//         });
+//         const response = NextResponse.json({ message: 'user logged out successfully' });
+//         response.cookies.delete('token');
+//         return response;
 //     } catch (error) {
 //         return Response.json({ message: 'Internal server error', error }, { status: 500 });
 //     }
 // }
-
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    try {
-        const response = NextResponse.json({ message: 'user logged out successfully' });
-        response.cookies.delete('token');
-        return response;
-    } catch (error) {
-        return Response.json({ message: 'Internal server error', error }, { status: 500 });
-    }
-}

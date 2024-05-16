099e855f644fb4d707ee7dac0171fc2c46de6e4e
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2024-05-16 14:32:21 +0330

    using NextResponse

--- src/app/api/auth/logOut/route.ts
+++ src/app/api/auth/logOut/route.ts
@@ -1,24 +1,24 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    try {
-        return NextResponse.json({ message: 'user logged out successfully' }, {
-            status: 200,
-            headers: { 'Set-Cookie': 'token=; HttpOnly; Path=/; Max-Age=0' }
-        });
-    } catch (error) {
-        return NextResponse.json({ message: 'Internal server error', error }, { status: 500 });
-    }
-}
-
 // import { NextResponse } from "next/server";
 
-// export async function GET() {
+// export async function GET(request: Request) {
 //     try {
-//         const response = NextResponse.json({ message: 'user logged out successfully' });
-//         response.cookies.delete('token');
-//         return response;
+//         return NextResponse.json({ message: 'user logged out successfully' }, {
+//             status: 200,
+//             headers: { 'Set-Cookie': 'token=; HttpOnly; Path=/; Max-Age=0' }
+//         });
 //     } catch (error) {
-//         return Response.json({ message: 'Internal server error', error }, { status: 500 });
+//         return NextResponse.json({ message: 'Internal server error', error }, { status: 500 });
 //     }
 // }
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({ message: 'user logged out successfully' });
+        response.cookies.delete('token');
+        return response;
+    } catch (error) {
+        return Response.json({ message: 'Internal server error', error }, { status: 500 });
+    }
+}
